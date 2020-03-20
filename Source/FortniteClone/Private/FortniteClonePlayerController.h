9c52290086ab8912fa64b8bc9a6520f64853c3af
pivotman319 <pivotman2003@gmail.com>
2020-03-20 14:19:02 +0100

    Defining new log category
    `LogMyGame` obsoleted w/ `LogFortniteClonePlayerController`

---
+++ Source/FortniteClone/Private/FortniteClonePlayerController.h
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "FortniteClonePlayerController.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogFortniteClonePlayerController, Log, All);
+
+class AFortniteCloneSpectator;
+class AStormActor;
+class AGameMode;
+class AFortniteClonePlayerState;
+/**
+ * 
+ */
+UCLASS()
+class FORTNITECLONE_API AFortniteClonePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	AFortniteClonePlayerController();
+
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerSwitchToSpectatorMode();
+
+	bool Initialized;
+
+	FString PlayerSessionId;
+
+	AStormActor* CurrentStorm;
+
+	TSubclassOf<AFortniteCloneSpectator> PlayerSpectatorClass;
+
+	UPROPERTY(Replicated)
+	int PlayerCount;
+
+	UPROPERTY(Replicated)
+	int SpectatorCount;
+
+	UFUNCTION(BlueprintPure, Category = "Count")
+	int GetPlayerCount();
+		
+	UFUNCTION(BlueprintPure, Category = "Count")
+	int GetSpectatorCount();
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerGetNumPlayers();
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerGetNumSpectators();
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerUpdateCountAfterDeath();
+
+	UFUNCTION(BlueprintPure, Category = "Count")
+	int GetKillCount();
+
+	UPROPERTY(Replicated)
+	bool SpawnAsSpectator;
+
+	virtual bool IsSupportedForNetworking() const override
+	{
+		return true;
+	}
+
+private:
+	virtual void BeginPlay() override;
+
+};
