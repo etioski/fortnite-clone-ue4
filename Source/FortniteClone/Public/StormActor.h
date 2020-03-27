699f42846661093fd26f91dc069ad5de57cc350c
pivotman319 <pivotman2003@gmail.com>
2020-03-27 14:08:49 +0100

    Optimized storm actor
    optimized version from @RetroGamesEngineer

--- Source/FortniteClone/Public/StormActor.h
+++ Source/FortniteClone/Public/StormActor.h
@@ -1,12 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Weekly - open-source on GitHub!
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "StormActor.generated.h"
-
-
 
 UCLASS()
 class FORTNITECLONE_API AStormActor : public AActor
@@ -16,6 +14,7 @@ class FORTNITECLONE_API AStormActor : public AActor
 public:	
 	// Sets default values for this actor's properties
 	AStormActor();
+	void InitializeStorm();
 
 protected:
 	// Called when the game starts or when spawned
@@ -25,14 +24,68 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY()
+	FTimerHandle StormSetupTimerHandle;
+
+	UPROPERTY()
+	FTimerHandle StormStateTimerHandle;
+	
+	UPROPERTY()
+	FTimerHandle StormDamageTimerHandle;
+
 	UPROPERTY(Replicated)
 	float Damage;
+
+	UPROPERTY(Replicated)
+	float StormAdvanceStageRate;
+
+	UPROPERTY(Replicated)
+	float StormIncreaseDamageRate;
 
 	UPROPERTY(Replicated)
 	bool IsShrinking;
 
 	UPROPERTY(Replicated)
+	FVector InitialSizeScale;
+	
+	UPROPERTY(Replicated)
+	FVector InitialActorLocation;
+
+	UPROPERTY(Replicated)
 	FVector SizeScale;
+
+	UPROPERTY(Replicated)
+	TArray<FVector> SizeScales;
+
+	UPROPERTY(Replicated)
+	int32 ScaleIndex;
+
+	UPROPERTY(Replicated)
+	int32 ScaleTotalCount;
+
+	UPROPERTY()
+	float ScaleDownRate;
+
+	UPROPERTY()
+	float InverseScaleDownRate;
+
+	UPROPERTY()
+	float ScaleHighThreshold;
+
+	UPROPERTY()
+	float ScaleMidThreshold;
+
+	UPROPERTY()
+	float ScaleLowThreshold;
+
+	UPROPERTY()
+	float ScaleHighModifier;
+
+	UPROPERTY()
+	float ScaleMidModifier;
+
+	UPROPERTY()
+	float ScaleLowModifier;
 
 	UPROPERTY()
 	int Stage;
@@ -43,12 +96,12 @@ public:
 	}
 
 	UFUNCTION(Server, Reliable, WithValidation)
-	void ServerSetIsShrinking();
+	void AdvanceStage();
 
 	UFUNCTION(Server, Reliable, WithValidation)
 	void ServerSetNewDamage();
 
-	UFUNCTION(Server, Reliable, WithValidation)
+	UFUNCTION(Server,Reliable,WithValidation)
 	void ServerStartStorm();
 
 };
